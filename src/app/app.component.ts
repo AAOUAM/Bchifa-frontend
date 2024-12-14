2735f25bb6729f34905ae40949ac0ef04d58f36c
Aymanek24 <aymanekenbouch@gmail.com>
2024-12-14 13:46:58 +0100

    aymane vedio call interface , camera micro end call

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,12 +6,13 @@ import {HomeComponent} from './home/home.component';
 import {InfoComponent} from './info/info.component';
 import {DocumentsComponent} from './documents/documents.component';
 import {FormsModule} from '@angular/forms';
+import {VediocallComponent} from './vediocall/vediocall.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ RouterOutlet, NavbarComponent, FooterComponent, HomeComponent, InfoComponent, DocumentsComponent,FormsModule],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent, HomeComponent, InfoComponent, DocumentsComponent, FormsModule, VediocallComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
